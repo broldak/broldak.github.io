eb1d9d3a037571f5627e064a6761766d0413763f
Brian Oldak <brian@swantide.com>
2025-02-26 21:05:30 -0800

    try to fix not found

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,32 +5,21 @@ import markdownStyles from "@/app/components/markdown-styles.module.css";
 import Link from "next/link";
 
 const Post = async (props: { params: { slug: string } }) => {
-  try {
-    const params = await props.params;
-    const post = getPostBySlug(params.slug);
+  const params = await props.params;
+  const post = getPostBySlug(params.slug);
+  const content = await markdownToHtml(post.content || "");
 
-    if (!post) {
-      console.log("Post not found");
-      return <div>POST NOT FOUND</div>;
-    }
-
-    const content = await markdownToHtml(post.content || "");
-
-    return (
-      <div className="w-[80%]">
-        <div className="mb-4">
-          <Link href="/blog">&lt; Back to Blog</Link>
-        </div>
-        <article>
-          <h1 className="text-4xl font-bold">{post.title}</h1>
-          <div className={markdownStyles["markdown"]} dangerouslySetInnerHTML={{ __html: content }} />
-        </article>
+  return (
+    <div className="w-[80%]">
+      <div className="mb-4">
+        <Link href="/blog">&lt; Back to Blog</Link>
       </div>
-    );
-  } catch {
-    console.log("Post not found - Error");
-    return <div>POST NOT FOUND - ERROR</div>;
-  }
+      <article>
+        <h1 className="text-4xl font-bold">{post.title}</h1>
+        <div className={markdownStyles["markdown"]} dangerouslySetInnerHTML={{ __html: content }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
