f8380a92935586783538cada8427d0b1fb8e7257
Brian Oldak <brian@swantide.com>
2025-02-26 21:15:20 -0800

    add static params

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,11 @@
-import { getPostBySlug } from "@/lib/api";
+import { getAllPosts, getPostBySlug } from "@/lib/api";
 import markdownToHtml from "@/lib/markdownToHtml";
 import { notFound } from "next/navigation";
 import markdownStyles from "@/app/components/markdown-styles.module.css";
 import Link from "next/link";
+import { Metadata } from "next";
 
-const Post = async (props: { params: { slug: string } }) => {
+const Post = async (props: Params) => {
   const params = await props.params;
   const post = getPostBySlug(params.slug);
   const content = await markdownToHtml(post.content || "");
@@ -23,3 +24,38 @@ const Post = async (props: { params: { slug: string } }) => {
 };
 
 export default Post;
+
+type Params = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+  const params = await props.params;
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Brian's Blog`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
